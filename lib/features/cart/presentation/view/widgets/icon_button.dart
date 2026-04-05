5a948e580595f36d4a6a6dfb753692d37ff10038
Abdulrahman <syamabdo382@gmail.com>
2026-04-05 16:42:53 +0200

    create IconButton

---
+++ lib/features/cart/presentation/view/widgets/icon_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:mini_ecommerce/core/theme/app_colors.dart';
+
+class IconButton extends StatelessWidget {
+  const IconButton({
+    super.key,
+    required this.icon,
+    required this.onTap,
+    required this.filled,
+  });
+  final IconData icon;
+  final VoidCallback onTap;
+  final bool filled;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 28,
+        height: 28,
+        decoration: BoxDecoration(
+          color: filled ? AppColors.primary : Colors.transparent,
+          shape: BoxShape.circle,
+          border: filled ? null : Border.all(color: AppColors.border),
+        ),
+        child: Icon(
+          icon,
+          size: 14,
+          color: filled ? Colors.white : AppColors.textPrimary,
+        ),
+      ),
+    );
+  }
+}
