e7383bf2cb8eba93230b95820b5e532895dd5c27
Abdulrahman <syamabdo382@gmail.com>
2026-04-05 17:48:03 +0200

    order view

---
+++ lib/features/order/presentation/views/order_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:mini_ecommerce/features/order/presentation/views/widgets/order_view_body.dart';
+
+class OrderView extends StatelessWidget {
+  const OrderView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return OrderViewBody();
+  }
+}
---
+++ lib/features/order/presentation/views/widgets/order_view_body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class OrderViewBody extends StatelessWidget {
+  const OrderViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        
+      ],
+    );
+  }
+}
