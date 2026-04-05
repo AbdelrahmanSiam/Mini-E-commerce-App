46008af5c5156899083ae018ab518bf2b6778af1
Abdulrahman <syamabdo382@gmail.com>
2026-04-05 10:33:38 +0200

    Add Bloc listener

--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mini_ecommerce/core/di/setup_service_locator.dart';
+import 'package:mini_ecommerce/core/widgets/app_snack_bar.dart';
 import 'package:mini_ecommerce/features/auth/presentation/manager/cubits/auth_cubit/auth_cubit.dart';
 import 'package:mini_ecommerce/features/auth/presentation/views/widgets/login_view_body.dart';
 
@@ -12,7 +13,16 @@ class LoginView extends StatelessWidget {
     return Scaffold(
       body: BlocProvider(
         create: (context) => getIt<AuthCubit>(),
-        child: LoginViewBody(),
+        child: BlocListener<AuthCubit, AuthState>(
+          listener: (context, state) {
+            if (state is AuthSuccessState) {
+              // go to products
+            } else if (state is AuthFailureState) {
+              AppSnackBar.showError(context, state.errMessage);
+            }
+          },
+          child: LoginViewBody(),
+        ),
       ),
     );
   }
