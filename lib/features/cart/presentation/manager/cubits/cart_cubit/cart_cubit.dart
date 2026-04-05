4d73e3e9ff7d7808f5b106537e8682a54df7ef1f
Abdulrahman <syamabdo382@gmail.com>
2026-04-05 16:59:30 +0200

    create CartCubit methods

--- lib/features/cart/presentation/manager/cubits/cart_cubit/cart_cubit.dart
+++ lib/features/cart/presentation/manager/cubits/cart_cubit/cart_cubit.dart
@@ -1,9 +1,67 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mini_ecommerce/features/cart/domain/entites/cart_item_entity.dart';
 
 part 'cart_state.dart';
 
 class CartCubit extends Cubit<CartState> {
   CartCubit() : super(CartInitialState());
+
+  final List<CartItemEntity> _items = [];
+
+  List<CartItemEntity> get items => List.unmodifiable(_items);
+
+  void addItem(CartItemEntity item) {
+    final index = _items.indexWhere((e) => e.id == item.id);
+    if (index != -1) {
+      _items[index] = _items[index].copyWith(
+        quantity: _items[index].quantity + 1,
+      );
+    } else {
+      _items.add(item);
+    }
+    _emitUpdated();
+  }
+
+  void removeItem(int id) {
+    _items.removeWhere((e) => e.id == id);
+    _emitUpdated();
+  }
+
+  void increaseQuantity(int id) {
+    final index = _items.indexWhere((e) => e.id == id);
+    if (index != -1) {
+      _items[index] = _items[index].copyWith(
+        quantity: _items[index].quantity + 1,
+      );
+      _emitUpdated();
+    }
+  }
+
+  void decreaseQuantity(int id) {
+    final index = _items.indexWhere((e) => e.id == id);
+    if (index != -1) {
+      if (_items[index].quantity <= 1) {
+        _items.removeAt(index);
+      } else {
+        _items[index] = _items[index].copyWith(
+          quantity: _items[index].quantity - 1,
+        );
+      }
+      _emitUpdated();
+    }
+  }
+
+  void clearCart() {
+    _items.clear();
+    _emitUpdated();
+  }
+
+  void _emitUpdated() {
+    final total = _items.fold<double>(
+      0,
+      (sum, item) => sum + (item.price * item.quantity),
+    );
+    emit(CartUpdatedState(items: List.from(_items), total: total));
+  }
 }
